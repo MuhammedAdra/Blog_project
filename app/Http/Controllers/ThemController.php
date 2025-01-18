34fe90267f3ef215913236a23ab7218c5f680b56
Muhammed Adra <muhammedadra.2000@gmail.com>
2025-01-18 15:03:16 +0300

    Theme to views

---
+++ app/Http/Controllers/ThemController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ThemController extends Controller
+{
+    public function index(){
+        return view('them.index');
+
+    }
+    public function category(){
+        return view('them.category');
+
+    }
+    public function contact(){
+        return view('them.contact');
+
+    }
+
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,28 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ThemController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(ThemController::class)->name('them.')->group(function(){
+    Route::get('/', 'index')->name('index');
+    Route::get('/category', 'category')->name('category');
+    Route::get('/contact','contact')->name('contact');
+
+
 });
+
+
+
+
+
+
+
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
